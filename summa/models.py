e3d7ff7e110aca7fff80c36bdc46412c6cca8a72
Igor Brown <ibrownramos@gmail.com>
2021-06-07 18:22:29 -0300

    fix: bug fixes cnpj and empresa search

--- summa/models.py
+++ summa/models.py
@@ -67,7 +67,7 @@ class Campus(models.Model):
     estado = models.ForeignKey(Estado, on_delete=models.DO_NOTHING)
     telefone = models.CharField('Telefone', max_length=16)
     site = models.URLField('Site', max_length=155)
-    email = models.EmailField('E-mail', max_length=155, blank=True, null=True)
+    email = models.EmailField('E-mail', max_length=155)
     instituicao = models.ForeignKey(Instituicao, related_name='campus', on_delete=models.DO_NOTHING)
     logo = models.FileField('Logo Instituição', upload_to=get_file_path)
     modelo_certificado = models.FileField('Imagem de fundo certificado (1080x755 px)', upload_to=get_file_path)
@@ -161,7 +161,7 @@ class AtividadeComplementar(models.Model):
                                   auto_choose=False,
                                   sort=False)
     descricao = models.CharField('Descrição', max_length=255)
-    empresa = models.CharField('Empresa/Instituição', max_length=155)
+    empresa = models.CharField('Empresa/Instituição', max_length=255)
     cnpj = models.CharField('CNPJ', max_length=18, blank=True)
     carga_horaria_informada = models.IntegerField('Carga Horária Informada')
     carga_horaria_integralizada = models.IntegerField('Carga Horária Integralizada', default=0, null=True, blank=True)
